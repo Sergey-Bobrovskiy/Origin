6d3b2d93b873a3887f49a7d2d3f728330ad5aa5a
Bobrovskij <79031844536@mail.ru>
2023-06-26 22:08:33 +0300

    For

---
+++ Desktop/Examples/Exampe010_MethodArray/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] array = {1,12,31,41,43,56,41,8};
+
+int  n = array.Length;
+int find =41;
+int index = 0;
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
